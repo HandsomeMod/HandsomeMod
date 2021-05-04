5cf8235ba88a00370d28e029f6536fb79c08c950
handsomeyingyan <handsomeyingyan@github.com>
2021-05-04 12:32:29 +0800

    [linux-msm8916] more fixes

--- target/linux/msm89xx/msm8916/target.mk
+++ target/linux/msm89xx/msm8916/target.mk
@@ -5,10 +5,13 @@
 # See /LICENSE for more information.
 #
 
+include $(TOPDIR)/rules.mk
+
 SUBTARGET:=msm8916
 BOARDNAME:=Msm8916 Soc based Targets
 CPU_TYPE:=cortex-a53
 CPU_SUBTYPE:=neon
+ARCH:=aarch64
 
 define Target/Description
 	Build firmware images for Qualcomm Msm8916 based Targets.
