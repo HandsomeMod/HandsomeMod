f158201e17dcf484453927b1808d84f898b27635
HandsomeYingyan <handsomeyingyan@gmail.com>
2021-12-12 16:21:09 +0800

    [target] msm89xx : refresh image makefile

--- target/linux/msm89xx/image/msm8916.mk
+++ target/linux/msm89xx/image/msm8916.mk
@@ -1,8 +1,6 @@
+# SPDX-License-Identifier: GPL-2.0-only
 #
 # Copyright (C) 2020-2021 HandsomeMod Project
-#
-# This is free software, licensed under the GNU General Public License v2.
-# See /LICENSE for more information.
 #
 
 ifeq ($(SUBTARGET),msm8916)
@@ -10,20 +8,19 @@ ifeq ($(SUBTARGET),msm8916)
 define Device/msm8916
   $(Device/Qcom)
   SOC := msm8916
-endef
-
-# RedMi 2
-define Device/XiaoMi_wingtech-wt88047-modem
-  $(Device/msm8916)
-  DEVICE_VENDOR := XiaoMi
-  DEVICE_MODEL := Redmi 2
-  QCOM_CMDLINE := "earlycon console=tty0 console=ttyMSM0,115200 root=/dev/mmcblk1p23 rw"
+  QCOM_CMDLINE := "earlycon console=ttyMSM0,115200 root=PARTUUID=a7ab80e8-e9d1-e8cd-f157-93f69b1d141e rw"
   QCOM_BOOTIMG_FLASH_OFFSET_BASE := 0x80000000
   QCOM_BOOTIMG_FLASH_OFFSET_KERNEL := 0x00080000
   QCOM_BOOTIMG_FLASH_OFFSET_RAMDISK := 0x02000000
   QCOM_BOOTIMG_FLASH_OFFSET_SECOND := 0x00f00000
   QCOM_BOOTIMG_FLASH_OFFSET_TAGS := 0x01e00000
   QCOM_BOOTIMG_FLASH_OFFSET_PAGESIZE := 2048
+endef
+
+define Device/XiaoMi_wingtech-wt88047-modem
+  $(Device/msm8916)
+  DEVICE_VENDOR := XiaoMi
+  DEVICE_MODEL := Redmi 2
 endef
 
 TARGET_DEVICES += XiaoMi_wingtech-wt88047-modem
